4e9e03dd9943b37835c38113c9e4f0382b380baf
tomii7 <108472900+tomiicr7@users.noreply.github.com>
2022-09-08 12:14:44 -0300

    Update 5en_linea.py

--- 5en_linea.py
+++ 5en_linea.py
@@ -1,38 +1,42 @@
 import tkinter
+from tkinter import *
 from tkinter import PhotoImage
 from PIL import Image,ImageTk
 from functools import partial
 
-ventana = tkinter.Tk()
-ventana.title("5 En Linea")
+class Celinea:
+    def __init__(self):
+        self.ventana = tkinter.Tk()
+        self.ventana.title("5 En Linea")
+        img = Image.open("ficharoja.png")
+        img = img.resize((50, 60), Image.Resampling.LANCZOS)
+        self.img = ImageTk.PhotoImage(img)
+        img2 = Image.open("fazul.jpg")
+        img2 = img2.resize((50, 60), Image.Resampling.LANCZOS)
+        self.img2 = ImageTk.PhotoImage(img2)
+        self.tabla_fichas = []
+        self.ficharoja = True
+        self.fichaazul = False
+        for i in range(6):
+            tabla_fichas = []    
+            for j in range(7):
+                b = tkinter.Button(self.ventana,  command=partial(self.agre_ficha, i, j), height=5, width=5)
+                b.grid(row=i, column=j)
+                tabla_fichas.append(b)
+            self.tabla_fichas.append(tabla_fichas)
 
-img = Image.open("ficharoja.png")
-img = resize((50, 60), Image.resampling.LANCZOS)
-img = ImageTk.PhotoImage(img)
+        print(self.tabla_fichas)    
+        self.ventana.mainloop()
 
-img2 = Image.open("fazul.jpg")
-img2 = resize((50, 60), Image.resampling.LANCZOS)
-img2 = ImageTk.PhotoImage(img)
+    def agre_ficha(self, col, fila):
+        if self.ficharoja:
+            imagen = img
+        else:
+            imagen = img2
+        self.tabla_ficha[col][fila].config
+        self.ficharoja = not self.ficharoja
+        self.tabla_fichas =[]
+        x = True
 
-
-def agre_ficha(col, fila):
-    global x
-    if x:
-        imagen = img
-    else:
-        imagen = img2
-        
-tabla_fichas =[]
-x = true
-
-for i in range(7):
-    tabla_fichas = []    
-    for j in range(4):
-        b = tkinter.Button(ventana,  command=partial(agre_ficha, i, j), height=4, width=5)
-        b.grid(row=i, column=j)
-        fila_botones.append(b)
-    tabla_botones.append(tabla_fichas)
-    
-ventana.mainloop
-
+c = Celinea()
 
